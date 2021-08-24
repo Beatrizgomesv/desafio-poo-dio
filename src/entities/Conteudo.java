cb7d1e172306a3504af8ba77d407a51847dad1a9
Beatrizgomesv <82125152+Beatrizgomesv@users.noreply.github.com>
2021-08-23 22:33:01 -0300

    first commit

---
+++ src/entities/Conteudo.java
@@ -0,0 +1,33 @@
+package entities;
+
+public abstract class Conteudo {
+
+protected static final double XP_PADRAO = 10;
+
+private String titulo;
+private String descricao;
+
+public abstract double calcularXp();
+
+public String getTitulo() {
+	return titulo;
+}
+
+public void setTitulo(String titulo) {
+	this.titulo = titulo;
+}
+
+public String getDescricao() {
+	return descricao;
+}
+
+public void setDescricao(String descricao) {
+	this.descricao = descricao;
+}
+
+public double caucularXp() {
+	// TODO Auto-generated method stub
+	return 0;
+}
+	
+}
